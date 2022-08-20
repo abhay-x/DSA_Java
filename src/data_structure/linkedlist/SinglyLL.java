7c0fcaad97f96b3399d3306277edd2cfa353765a
Abhay raj <abhayraj7631@gmail.com>
2022-08-21 00:00:19 +0530

    SLL insertion complete

--- src/data_structure/linkedlist/SinglyLL.java
+++ src/data_structure/linkedlist/SinglyLL.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class SLL {
     private Node head = null, tail = null; //only reference variable created which stores ref/address of objects
-    private int size;
+    public int size;
 
     public SLL() {
         this.size = 0;
@@ -27,23 +27,69 @@ class SLL {
         }
     }
 
-    public void insertFirst() {
+    public Node createNode() {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter value: ");
         int val = s.nextInt();
         Node node = new Node();
         node.val = val;
         node.next = null;
+        return node;
+    }
+
+    public void insertFirst() {
+        Node node = createNode();
         if (head == null) {
             head = node;
+            tail = node;
         } else {
             node.next = head;
             head = node;
         }
-        size += 1;
+        size++;
+    }
+
+    public void insertLast() {
+        Node node = createNode();
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        size++;
+    }
+
+    public void insertPosition() {
+        System.out.println("Enter the Position");
+        Scanner scn = new Scanner(System.in);
+        int position = scn.nextInt();
+        if (position < 0 || position > size) {
+            System.out.println("Invalid Position\n");
+            return;
+        }
+        if (position == 0) {
+            insertFirst();
+            return;
+        }
+        if (position == size) {
+            insertLast();
+            return;
+        }
+        Node temp = head;
+        for (int count = 1; count != position; count++) {
+            temp = temp.next;
+            count++;
+        }
+        Node node = createNode();
+        node.next = temp.next;
+        temp.next = node;
+        size++;
     }
 
     void display() {
-        if(head==null) {
+        if (head == null) {
             System.out.println("Linked List is Empty");
             return;
         }
@@ -62,7 +108,8 @@ public class SinglyLL {
         Scanner s = new Scanner(System.in);
         while (true) {
             System.out.println("Choose from the option Menu:-");
-            System.out.println("0.Exit\n1.Display\n2.Insert At First\n3.Delete At First");
+            System.out.println("0.Exit\n1.Display\n2.Size of Linked List\n3.Insert At First\n" +
+                    "4.Insert at Last\n5.Insert At Position\nDelete At First");
             int option = s.nextInt();
             switch (option) {
                 case 0:
@@ -72,10 +119,27 @@ public class SinglyLL {
                     l.display();
                     break;
                 case 2:
+                    System.out.println(l.size + " Elements.");
+                    break;
+                case 3:
                     System.out.print("Enter the no of items:");
-                    int times = s.nextInt();
-                    for (int i = 0; i < times; i++) {
+                    int t1 = s.nextInt();
+                    for (int i = 0; i < t1; i++) {
                         l.insertFirst();
+                    }
+                    break;
+                case 4:
+                    System.out.print("Enter the no of items:");
+                    int t2 = s.nextInt();
+                    for (int i = 0; i < t2; i++) {
+                        l.insertLast();
+                    }
+                    break;
+                case 5:
+                    System.out.print("Enter the no of items:");
+                    int t3 = s.nextInt();
+                    for (int i = 0; i < t3; i++) {
+                        l.insertPosition();
                     }
                     break;
                 default:
